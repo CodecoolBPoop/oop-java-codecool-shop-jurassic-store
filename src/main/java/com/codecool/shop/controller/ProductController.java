a5472fddf66bc56cd4da83f6cd4c0df5212089f3
Tamas Frei <frei.tamas555@gmail.com>
2019-01-24 15:47:49 +0100

    Delete current category title

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -38,7 +38,6 @@ public class ProductController extends HttpServlet {
         context.setVariable("suppliers", supplierDataStore.getAll());
 
         if (req.getParameter("category") == null && req.getParameter("supplier") == null) {
-            context.setVariable("category", null);
             context.setVariable("products", productDataStore.getAll());
         } else if (req.getParameter("category")!= null && req.getParameter("supplier") == null){
             filterByCategory(context, req);
